2907c3cc01e968037e52de38b812f1975bb1f6fe
Vaibhav Totla <100551411+vaibhavtotla5520@users.noreply.github.com>
2024-01-10 11:10:01 +0530

    Update Requests.php

---
+++ Config/Requests.php
@@ -0,0 +1,7 @@
+<?php
+// Request Handler
+require "Constants.php";
+
+if(isset($_POST["submit"] && $_POST["ACCESS_TOKEN"] == ACCESS_TOKEN) {
+   // call to the required controller using require_once "filename.php"
+}
