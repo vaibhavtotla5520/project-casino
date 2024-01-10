26f4d1f9cca1edb42eeb73ff5e9f456ccfafac94
Vaibhav Totla <100551411+vaibhavtotla5520@users.noreply.github.com>
2024-01-10 12:04:37 +0530

    Update AdminModel.php

--- Models/AdminModel.php
+++ Models/AdminModel.php
@@ -1,6 +1,6 @@
 <?php
-require_once "Config/Database.php";
-require_once "Config/Constants.php";
+require_once "../Config/Database.php";
+require_once "../Config/Constants.php";
 
 class AdminModel extends Database {
   
